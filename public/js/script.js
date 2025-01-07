c21054297879164e9729fbdd3c23fe6f56c307c2
Ajai-3 <ajaipjayan@gmail.com>
2025-01-07 10:31:34 +0530

    product page updated

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,7 @@
 window.onload = function () {
      window.scrollTo(0, 0);
 }; 
+
 // Sign-Up Form Elements
 const nameInput = document.getElementById('name');
 const emailInput = document.getElementById('email');
